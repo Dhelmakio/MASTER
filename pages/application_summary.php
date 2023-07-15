6b7c1d65a280be31dda1e23602f8db392c30f973
Dhel Mark Baylon <dhelmarksalidagabaylon@gmail.com>
2023-07-15 14:10:01 +0800

    added registry application summary and fixed minor errors

---
+++ pages/application_summary.php
@@ -0,0 +1,290 @@
+<!-- Signature Pad Modal END -->
+<div class="modal fade" id="sig_pad_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"
+    data-backdrop="static">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <h2>Application Summary</h2>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="panel tabbed-panel panel-default">
+                            <!-- <div class="panel-body"> -->
+                            <!-- <div class="panel-group" id="accordion"> -->
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">
+                                    <center>
+                                        <h4>
+                                            Personal Information
+                                        </h4>
+                                    </center>
+                                </div>
+                                <div id="collapseOne" class="panel-collapse collapse in">
+                                    <div class="panel-body">
+
+                                        <!-- <div class="col-lg-12">
+                                                <p class="lead">PERSONAL INFORMATION</p><br>
+                                            </div> -->
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Name:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_name"></span></u>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Address:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_address"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Gender:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_gender"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Date of Birth:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_dob"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Place of Birth:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_pob"></span></u>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Marital Status:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_mstatus"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Contact No.:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_contact"></span></u>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <hr>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <center>
+                                                    <p class="lead">FAMILY BACKGROUND</p>
+                                                </center>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Mother's Name:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_mother"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Contact No.:</label>
+                                            </div>
+                                            <div class="col-lg-6"><u><span id="disp_mcontact"></span></u>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Father's Name:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_father"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Contact No.:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_fcontact"></span></u>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Living With:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_lw"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <hr>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <center>
+                                                    <p class="lead">SPOUSE'S INFORMATION</p>
+                                                </center>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Name:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_spouse"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Contact No.:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_scontact"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Date of Birth:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_sdob"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Place of Birth:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_spob"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Address:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_saddress"></span></u>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Occupation:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_soccupation"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <hr>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="panel tabbed-panel panel-default">
+                            <!-- <div class="panel-body"> -->
+                            <!-- <div class="panel-group" id="accordion"> -->
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">
+                                    <center>
+                                        <h4>
+                                            Employment Information
+                                        </h4>
+                                    </center>
+                                </div>
+                                <div id="collapseOne" class="panel-collapse collapse in">
+                                    <div class="panel-body">
+
+                                        <!-- <div class="col-lg-12">
+                                                <p class="lead">EMPLOYMENT INFORMATION</p><br>
+                                            </div> -->
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Emp. / Agency:</label>
+                                            </div>
+                                            <div class="col-lg-6"><u><span id="disp_agency"></span></u>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Sector Type:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_sector"></span></u>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Business Type:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_tob"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Address:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_comaddress"></span></u>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Assigned Location:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_location"></span></u>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Name of Supervisor:</div>
+                                            <div class="col-lg-6"><u><span id="disp_wsupervisor"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Contact No.:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_wscontact"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Date Hired:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_hired"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Employment Status:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_estatus"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Mode of Salary:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_mos"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Bank Name:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_bank"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Type of ATM Card:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_atm"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Have other loan?</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_loan"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Gross Monthly:</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_gross"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>Other source of income:</label>
+                                            </div>
+                                            <div class="col-lg-6"><u><span id="disp_osincome"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6 text-right"><label>(Specify):</label></div>
+                                            <div class="col-lg-6"><u><span id="disp_os"></span></u></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <hr>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.panel -->
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 text-justify">
+                        <span>
+                            I hereby certify that the information provided in this form is complete, true and correct to
+                            the best of my knowledge.<b>I hold Una Amigo Lending Corporation</b> free
+                            from liability from any law
+                            particularly the Data
+                            Privacy Act CRA 10173. I
+                            also confirm that <b>Una
+                                Amigo
+                                Lending
+                                Corporation</b>
+                            fully
+                            explained to me the
+                            contents
+                            of my loan application
+                            including its terms,
+                            rates
+                            and other important
+                            details
+                            in a language known and
+                            understood by me. <b>I
+                                agree</b>
+                            <input name="agree" id="agree" onclick="enableButton()" value="disagree"
+                                style="transform: scale(1.3);transform: translate(1px, 2px);" type="checkbox">
+                            <br><br>
+                        </span>
+                    </div>
+                </div>
+                    <label class="" for="">*Attach Client's Signature</label>
+                    <br />
+                    <div class="" id="signature"></div>
+                    <br />
+                    <br />
+                    <button class="btn btn-default" id="clear">Clear Signature</button>
+
+
+            </div>
+            <div class="modal-footer" style="padding: 5px;">
+                <form action="#" method="post">
+                    <button type="button" class="btn btn-default text-small" data-dismiss="modal">Cancel</button>
+                    <button type="button" name="confirm" id="confirm_sig"
+                        class="btn btn-primary text-small" disabled>Confirm</button>
+                </form>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+</div>
+<!-- Signature Pad Modal END -->
